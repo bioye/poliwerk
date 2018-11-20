937c576b8ed515d7f9c4016dd951a4cfdb579a47
Abioye Bankole <bioyexx@yahoo.com>
2018-11-20 18:22:15 +0100

    posgresql first pass

--- src/main/java/ng/nectar/configuration/MainConfig.java
+++ src/main/java/ng/nectar/configuration/MainConfig.java
@@ -12,11 +12,11 @@ public class MainConfig {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));//CLEARDB_DATABASE_URL
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+        String dbUrl = "jdbc:postgres://" + dbUri.getHost() + dbUri.getPath();//jdbc:mysql
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);

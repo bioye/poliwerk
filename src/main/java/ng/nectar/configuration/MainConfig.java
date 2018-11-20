ba7faa6e33c5541711534b89a7f8ffa1243aca0f
Abioye Bankole <bioyexx@yahoo.com>
2018-11-20 17:38:17 +0100

    implemented application-context.xml with cleardb configuration as stated in cleardb doc to fix 60s timeout on heroku

---
+++ src/main/java/ng/nectar/configuration/MainConfig.java
@@ -0,0 +1,28 @@
+package ng.nectar.configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}

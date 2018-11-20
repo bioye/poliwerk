84d60cff71f524cdfebcb41a2a43e280e36adc42
Abioye Bankole <bioyexx@yahoo.com>
2018-11-20 16:25:34 +0100

    maven update ImportResource

--- src/main/java/ng/nectar/PoliworksApplication.java
+++ src/main/java/ng/nectar/PoliworksApplication.java
@@ -2,11 +2,11 @@ package ng.nectar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-//@EnableJpaRepositories("ng.nectar.repository")
 @ImportResource("classpath:application-context.xml")
 public class PoliworksApplication {
 
